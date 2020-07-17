80224f96e5b076b53182f4959d94057d2aed3386
Ivan Savciuc <ivan.savciuc@gmail.com>
2020-07-17 14:35:34 +0300

    migration to new operator sdk project layout

---
+++ api/v1alpha1/project_types.go
@@ -0,0 +1,113 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ProjectSpec defines the desired state of Project
+type ProjectSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// ProjectStatus defines the observed state of Project
+type ProjectStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// +kubebuilder:validation:MaxLength=63
+	// +kubebuilder:validation:Format="^[a-zA-Z0-9_-]*$"
+	// Project name
+	Name string `json:"name"`
+
+	// +kubebuilder:validation:MaxLength=64
+	// Credit card ID
+	CardId string `json:"card_id,omitempty"`
+
+	// +kubebuilder:validation:MaxLength=32
+	// Account ID
+	AccountId string `json:"account_id,omitempty"`
+
+	// +kubebuilder:validation:MaxLength=1000
+	// Billing name and address of the project
+	BillingAddress string `json:"billing_address,omitempty"`
+
+	// +kubebuilder:validation:MaxItems=10
+	// +kubebuilder:validation:UniqueItems=true
+	// Billing contact emails of the project
+	BillingEmails []string `json:"billing_emails,omitempty"`
+
+	// +kubebuilder:validation:Enum=AUD;CAD;CHF;DKK;EUR;GBP;NOK;SEK;USD
+	// Billing currency
+	BillingCurrency string `json:"billing_currency,omitempty"`
+
+	// +kubebuilder:validation:MaxLength=1000
+	// Extra text to be included in all project invoices, e.g. purchase order or cost center number
+	BillingExtraText string `json:"billing_extra_text,omitempty"`
+
+	// +kubebuilder:validation:MinLength=2
+	// +kubebuilder:validation:MaxLength=2
+	// Billing country code of the project
+	CountryCode string `json:"country_code,omitempty"`
+
+	// +kubebuilder:validation:MaxLength=256
+	// Target cloud, example: aws-eu-central-1
+	Cloud string `json:"cloud,omitempty"`
+
+	// +kubebuilder:validation:MaxLength=63
+	// Project name from which to copy settings to the new project
+	CopyFromProject string `json:"copy_from_project,omitempty"`
+
+	// +kubebuilder:validation:MaxItems=10
+	// +kubebuilder:validation:UniqueItems=true
+	// Technical contact emails of the project
+	TechnicalEmails []string `json:"technical_emails,omitempty"`
+
+	// +kubebuilder:validation:MaxLength=64
+	// EU VAT Identification Number
+	VatId string `json:"vat_id,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// Project is the Schema for the projects API
+// +kubebuilder:subresource:status
+type Project struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ProjectSpec   `json:"spec,omitempty"`
+	Status ProjectStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ProjectList contains a list of Project
+type ProjectList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Project `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Project{}, &ProjectList{})
+}
