85f9ed3b0d4fe72f0b3decb800826ead932f4a9e
Ivan Savciuc <ivan.savciuc@gmail.com>
2021-02-16 18:10:59 +0200

    add support for kafka schema

---
+++ api/v1alpha1/kafkaschema_types.go
@@ -0,0 +1,84 @@
+// Copyright (c) 2020 Aiven, Helsinki, Finland. https://aiven.io/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// KafkaSchemaSpec defines the desired state of KafkaSchema
+type KafkaSchemaSpec struct {
+	// +kubebuilder:validation:MaxLength=63
+	// +kubebuilder:validation:Format="^[a-zA-Z0-9_-]*$"
+	// x-kubernetes-immutable: true
+	// Project to link the Kafka Schema to
+	Project string `json:"project"`
+
+	// +kubebuilder:validation:MaxLength=63
+	// x-kubernetes-immutable: true
+	// Service to link the Kafka Schema to
+	ServiceName string `json:"service_name"`
+
+	// +kubebuilder:validation:MaxLength=63
+	// x-kubernetes-immutable: true
+	// Kafka Schema Subject name
+	SubjectName string `json:"subject_name"`
+
+	// Kafka Schema configuration should be a valid Avro Schema JSON format
+	Schema string `json:"schema"`
+
+	// +kubebuilder:validation:Enum=BACKWARD;BACKWARD_TRANSITIVE;FORWARD;FORWARD_TRANSITIVE;FULL;FULL_TRANSITIVE;NONE
+	// Kafka Schemas compatibility level
+	CompatibilityLevel string `json:"compatibility_level,omitempty"`
+}
+
+// KafkaSchemaStatus defines the observed state of KafkaSchema
+type KafkaSchemaStatus struct {
+	// +kubebuilder:validation:MaxLength=63
+	// +kubebuilder:validation:Format="^[a-zA-Z0-9_-]*$"
+	// Project to link the Kafka Schema to
+	Project string `json:"project"`
+
+	// +kubebuilder:validation:MaxLength=63
+	// Service to link the Kafka Schema to
+	ServiceName string `json:"service_name"`
+
+	// +kubebuilder:validation:MaxLength=63
+	// Kafka Schema Subject name
+	SubjectName string `json:"subject_name"`
+
+	// Kafka Schema configuration should be a valid Avro Schema JSON format
+	Schema string `json:"schema"`
+
+	// +kubebuilder:validation:Enum=BACKWARD;BACKWARD_TRANSITIVE;FORWARD;FORWARD_TRANSITIVE;FULL;FULL_TRANSITIVE;NONE
+	// Kafka Schemas compatibility level
+	CompatibilityLevel string `json:"compatibility_level,omitempty"`
+
+	// Kafka Schema configuration version
+	Version int `json:"version"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// KafkaSchema is the Schema for the kafkaschemas API
+type KafkaSchema struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   KafkaSchemaSpec   `json:"spec,omitempty"`
+	Status KafkaSchemaStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// KafkaSchemaList contains a list of KafkaSchema
+type KafkaSchemaList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []KafkaSchema `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&KafkaSchema{}, &KafkaSchemaList{})
+}
