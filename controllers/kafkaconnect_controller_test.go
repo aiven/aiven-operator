08c6521749369cfe0899e7d4fe468f6ec60d9603
Ivan Savciuc <ivan.savciuc@gmail.com>
2021-03-18 08:33:08 +0200

    add kafka connect controller ginkgo tests

---
+++ controllers/kafkaconnect_controller_test.go
@@ -0,0 +1,102 @@
+package controllers
+
+import (
+	"context"
+	"github.com/aiven/aiven-k8s-operator/api/v1alpha1"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"os"
+	"time"
+)
+
+var _ = Describe("KafkaConnect Controller", func() {
+	// Define utility constants for object names and testing timeouts/durations and intervals.
+	const (
+		kafkaconnectNamespace = "default"
+
+		timeout  = time.Minute * 20
+		interval = time.Second * 10
+	)
+
+	var (
+		kafkaconnect *v1alpha1.KafkaConnect
+		serviceName  string
+		ctx          context.Context
+	)
+
+	BeforeEach(func() {
+		serviceName = "k8s-test-kafkaconnect-acc-" + generateRandomID()
+
+		kafkaconnect = &v1alpha1.KafkaConnect{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "k8s-operator.aiven.io/v1alpha1",
+				Kind:       "KafkaConnect",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      serviceName,
+				Namespace: kafkaconnectNamespace,
+			},
+			Spec: v1alpha1.KafkaConnectSpec{
+				Project:               os.Getenv("AIVEN_PROJECT_NAME"),
+				ServiceName:           serviceName,
+				Plan:                  "business-4",
+				CloudName:             "google-europe-west1",
+				MaintenanceWindowDow:  "monday",
+				MaintenanceWindowTime: "10:00:00",
+			},
+		}
+		ctx = context.Background()
+
+		By("Creating a new KafkaConnect CR instance")
+		Expect(k8sClient.Create(ctx, kafkaconnect)).Should(Succeed())
+
+		kcLookupKey := types.NamespacedName{Name: serviceName, Namespace: kafkaconnectNamespace}
+		createdKafkaConnect := &v1alpha1.KafkaConnect{}
+		// We'll need to retry getting this newly created KafkaConnect,
+		// given that creation may not immediately happen.
+		By("by retrieving Kafka Connect instance from k8s")
+		Eventually(func() bool {
+			err := k8sClient.Get(ctx, kcLookupKey, createdKafkaConnect)
+
+			return err == nil
+		}, timeout, interval).Should(BeTrue())
+
+		By("by waiting Kafka Connect service status to become RUNNING")
+		Eventually(func() string {
+			err := k8sClient.Get(ctx, kcLookupKey, createdKafkaConnect)
+			if err == nil {
+				return createdKafkaConnect.Status.State
+			}
+
+			return ""
+		}, timeout, interval).Should(Equal("RUNNING"))
+
+		By("by checking finalizers")
+		Expect(createdKafkaConnect.GetFinalizers()).ToNot(BeEmpty())
+	})
+
+	Context("Validating KafkaConnect reconciler behaviour", func() {
+		It("should create a new Kafka Connect service", func() {
+			createdKafkaConnect := &v1alpha1.KafkaConnect{}
+			kcLookupKey := types.NamespacedName{Name: serviceName, Namespace: kafkaconnectNamespace}
+
+			Expect(k8sClient.Get(ctx, kcLookupKey, createdKafkaConnect)).Should(Succeed())
+
+			// Let's make sure our KafkaConnect status was properly populated.
+			By("by checking that after creation KafkaConnect service status fields were properly populated")
+			Expect(createdKafkaConnect.Status.ServiceName).Should(Equal(serviceName))
+			Expect(createdKafkaConnect.Status.State).Should(Equal("RUNNING"))
+			Expect(createdKafkaConnect.Status.Plan).Should(Equal("business-4"))
+			Expect(createdKafkaConnect.Status.CloudName).Should(Equal("google-europe-west1"))
+			Expect(createdKafkaConnect.Status.MaintenanceWindowDow).Should(Equal("monday"))
+			Expect(createdKafkaConnect.Status.MaintenanceWindowTime).Should(Equal("10:00:00"))
+		})
+	})
+
+	AfterEach(func() {
+		By("Ensures that KafkaConnect instance was deleted")
+		ensureDelete(ctx, kafkaconnect)
+	})
+})
