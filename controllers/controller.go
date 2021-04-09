db6bff037305de9636c057970377774c4f0121d4
Ivan Savciuc <ivan.savciuc@gmail.com>
2021-04-09 17:52:56 +0300

    refactoring of controller layout

--- controllers/controller.go
+++ controllers/controller.go
@@ -6,38 +6,238 @@ import (
 	"github.com/aiven/aiven-go-client"
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"reflect"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"strings"
+	"time"
 )
 
-type Controller struct {
-	client.Client
-	Log         logr.Logger
-	Scheme      *runtime.Scheme
-	AivenClient *aiven.Client
-}
+const requeueTimeout = 10 * time.Second
+
+type (
+	// Controller reconciles the Aiven objects
+	Controller struct {
+		client.Client
+		Log         logr.Logger
+		Scheme      *runtime.Scheme
+		AivenClient *aiven.Client //TODO: remove it, left here for backwards compatibility
+	}
+
+	// Handlers represents Aiven API handlers
+	// It intended to be a layer between Kubernetes and Aiven API that handles all aspects
+	// of the Aiven services lifecycle.
+	Handlers interface {
+		// create creates an instance on the Aiven side.
+		// If entity already exists it should not error, but if it impossible to create it by
+		// any other reason it should return an error
+		create(logr.Logger, client.Object) (createdObj client.Object, error error)
+
+		// delete remove an instance on Aiven side.
+		// If an object already deleted and cannot be found, it should not error. Otherwise, retrieve an error.
+		// For example, if there is a secret associated with an instance, it should retrieve it to be deleted
+		// by controller. When deletion requires multiple runs, the bool parameter 'isDeleted' should be
+		// false and only when an entity was successfully deleted on the Aiven side should it be true.
+		delete(logr.Logger, client.Object) (objToBeDeleted client.Object, isDeleted bool, error error)
+
+		// exists checks if an instance already exists on the Aiven side. It should return true if it does
+		// exist and false when it does not.
+		exists(logr.Logger, client.Object) (exists bool, error error)
+
+		// update updates an instance on the Aiven side, assuming it was previously created.
+		update(logr.Logger, client.Object) (updatedObj client.Object, error error)
+
+		// getSecret retrieves a secret that is generated on the fly based on data from Aiven API.
+		// When not applicable to service, it should return nil.
+		getSecret(logr.Logger, client.Object) (secret *corev1.Secret, error error)
+
+		// checkPreconditions checks whether all preconditions are met for an Aiven service to be handled
+		// further down the road. For example, it is applicable when one instance is dependent on other to
+		// be created upfront. Or parent instance is in the transition state.
+		checkPreconditions(logr.Logger, client.Object) bool
+
+		// isActive checks if an instance is Active on the Aiven side. Applicable for services that have
+		// multiple states and in transition. When a service reaches a target state, return true.
+		isActive(logr.Logger, client.Object) (bool, error)
+	}
+)
+
+var aivenClient *aiven.Client
 
 // +kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs=get;list;create;update
 // +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
 
-// InitAivenClient retrieves an Aiven client
-func (c *Controller) InitAivenClient(req ctrl.Request, ctx context.Context, log logr.Logger) error {
-	if c.AivenClient != nil {
-		return nil
+func (c *Controller) reconcileInstance(h Handlers, ctx context.Context, log logr.Logger, req ctrl.Request, o client.Object, finalizerName string) (ctrl.Result, error) {
+	// Initiating Aiven client, secret with a token is required
+	if err := c.InitAivenClient(req, ctx, log); err != nil {
+		return ctrl.Result{}, err
+	}
+	c.AivenClient = aivenClient //TODO: remove it, left here for backwards compatibility
+
+	// Fetch an instance
+	err := c.Get(ctx, req.NamespacedName, o)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			c.Log.Info("Instance resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		c.Log.Error(err, "Failed to get Instance")
+		return ctrl.Result{}, err
 	}
 
-	var token string
+	// Check if the instance is marked to be deleted, which is
+	// indicated by the deletion timestamp being set.
+	isInstanceMarkedToBeDeleted := o.GetDeletionTimestamp() != nil
+	if isInstanceMarkedToBeDeleted {
+		if contains(o.GetFinalizers(), finalizerName) {
+			// Run finalization logic for finalizer. If the
+			// finalization logic fails, don't remove the finalize so
+			// that we can retry during the next reconciliation.
+			// When applicable, it retrieves an associated object that
+			// has to be deleted from Kubernetes, and it could be a
+			// secret associated with an instance.
+			objToBeDeleted, finalised, err := h.delete(log, o)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+
+			// Checking if instance was finalized, if not triggering a requeue
+			if !finalised {
+				return ctrl.Result{
+					Requeue:      true,
+					RequeueAfter: requeueTimeout,
+				}, nil
+			}
+
+			// Delete a K8s object if handler finalized things on Aiven side
+			if objToBeDeleted != nil {
+				if err := c.Delete(ctx, objToBeDeleted); err != nil {
+					return ctrl.Result{}, fmt.Errorf("cannot delete object: %w", err)
+				}
+			}
+
+			// Remove finalizer. Once all finalizers have been
+			// removed, the object will be deleted.
+			c.Log.Info("Removing finalizer from instance ...")
+			controllerutil.RemoveFinalizer(o, finalizerName)
+			err = c.Update(ctx, o)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+		return ctrl.Result{}, nil
+	}
+
+	// Add finalizer for this CR
+	if !contains(o.GetFinalizers(), finalizerName) {
+		if err := c.addFinalizer(o, finalizerName); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Check preconditions
+	if !h.checkPreconditions(log, o) {
+		return ctrl.Result{Requeue: true, RequeueAfter: requeueTimeout}, nil
+	}
+
+	// Check if instance already exists on Aiven side
+	exists, err := h.exists(log, o)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if !exists {
+		// If instance does not exist, create a new one
+		obj, err := h.create(log, o)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		// Get a secret if available
+		s, err := h.getSecret(log, o)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		// Creating a secret if available
+		if s != nil {
+			if err := c.Create(ctx, s); err != nil {
+				return ctrl.Result{}, err
+			}
+
+			err = controllerutil.SetControllerReference(o, s, c.Scheme)
+			if err != nil {
+				return ctrl.Result{}, fmt.Errorf("k8s set controller reference error %w", err)
+			}
+		}
+
+		return ctrl.Result{}, c.Status().Update(ctx, obj)
+	}
+
+	// Check if instance is already active
+	isActive, err := h.isActive(log, o)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// If instance is not active wait and try again
+	if !isActive {
+		return ctrl.Result{
+			Requeue:      true,
+			RequeueAfter: 10 * time.Second,
+		}, nil
+	}
+
+	// Updating instance on the Aiven side
+	obj, err := h.update(log, o)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if obj != nil { // If update functionality is available
+		// Updating a secret if available
+		s, err := h.getSecret(log, o)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		if s != nil {
+			if err := c.Update(ctx, s); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+
+		return ctrl.Result{}, c.Status().Update(ctx, obj)
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// InitAivenClient retrieves an Aiven client
+func (c *Controller) InitAivenClient(req ctrl.Request, ctx context.Context, log logr.Logger) error {
+	if aivenClient != nil {
+		return nil
+	}
+	log.Info("Initializing an Aiven Client ...")
 
 	// Check if aiven-token secret exists
+	var token string
 	secret := &corev1.Secret{}
+	log.Info("Getting an `aiven-token` secret from the namespace")
 	err := c.Get(ctx, types.NamespacedName{Name: "aiven-token", Namespace: req.Namespace}, secret)
 	if err != nil {
-		log.Error(err, "aiven-token secret is missing, required by the Aiven client")
-		return err
+		if errors.IsNotFound(err) {
+			log.Error(err, "aiven-token secret is missing, it is required by the Aiven client")
+		}
+		return fmt.Errorf("cannot get `aiven-token` secret: %w", err)
 	}
 
 	if v, ok := secret.Data["token"]; ok {
@@ -46,8 +246,28 @@ func (c *Controller) InitAivenClient(req ctrl.Request, ctx context.Context, log 
 		return fmt.Errorf("cannot initialize Aiven client, kubernetes secret has no `token` key")
 	}
 
-	c.AivenClient, err = aiven.NewTokenClient(token, "k8s-operator/")
+	if len(token) == 0 {
+		return fmt.Errorf("cannot initialize Aiven client, `token` key in a secret is empty")
+	}
+
+	log.Info("Creating an Aiven Client ...")
+	aivenClient, err = aiven.NewTokenClient(token, "k8s-operator/")
 	if err != nil {
+		return fmt.Errorf("cannot create an Aiven Client: %w", err)
+	}
+
+	log.Info("Aiven Client was successfully initialized")
+	return nil
+}
+
+func (c *Controller) addFinalizer(o client.Object, f string) error {
+	c.Log.Info("Adding Finalizer for the instance")
+	controllerutil.AddFinalizer(o, f)
+
+	// Update CR
+	err := c.Client.Update(context.Background(), o)
+	if err != nil {
+		c.Log.Error(err, "Failed to update instance with finalize")
 		return err
 	}
 
--- controllers/pg_controller.go
+++ controllers/pg_controller.go
@@ -9,20 +9,19 @@ import (
 	k8soperatorv1alpha1 "github.com/aiven/aiven-k8s-operator/api/v1alpha1"
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	"time"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
-
-const pgServiceFinalizer = "pg-service-finalizer.k8s-operator.aiven.io"
 
 // PGReconciler reconciles a PG object
 type PGReconciler struct {
 	Controller
+}
+
+// PGHandler handles an Aiven PG service
+type PGHandler struct {
+	Handlers
 }
 
 // +kubebuilder:rbac:groups=k8s-operator.aiven.io,resources=pgs,verbs=get;list;watch;create;update;patch;delete
@@ -30,143 +29,50 @@ type PGReconciler struct {
 
 func (r *PGReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := r.Log.WithValues("pg", req.NamespacedName)
+	log.Info("Reconciling Aiven PG")
 
-	if err := r.InitAivenClient(req, ctx, log); err != nil {
-		return ctrl.Result{}, err
-	}
-
-	// Fetch the PG instance
+	const pgServiceFinalizer = "pg-service-finalizer.k8s-operator.aiven.io"
 	pg := &k8soperatorv1alpha1.PG{}
-	err := r.Get(ctx, req.NamespacedName, pg)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			// Request object not token, could have been deleted after reconcile request.
-			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
-			// Return and don't requeue
-			log.Info("PG resource not token. Ignoring since object must be deleted")
-			return ctrl.Result{}, nil
-		}
-		// Error reading the object - requeue the request.
-		log.Error(err, "Failed to get PG")
-		return ctrl.Result{}, err
-	}
-
-	// Check if the PG instance is marked to be deleted, which is
-	// indicated by the deletion timestamp being set.
-	isProjectMarkedToBeDeleted := pg.GetDeletionTimestamp() != nil
-	if isProjectMarkedToBeDeleted {
-		if contains(pg.GetFinalizers(), pgServiceFinalizer) {
-			// Run finalization logic for projectFinalizer. If the
-			// finalization logic fails, don't remove the finalizer so
-			// that we can retry during the next reconciliation.
-			if err := r.finalizePGService(log, pg); err != nil {
-				return reconcile.Result{}, err
-			}
-
-			// Remove projectFinalizer. Once all finalizers have been
-			// removed, the object will be deleted.
-			controllerutil.RemoveFinalizer(pg, pgServiceFinalizer)
-			err := r.Client.Update(ctx, pg)
-			if err != nil {
-				return reconcile.Result{}, err
-			}
-		}
-		return reconcile.Result{}, nil
-	}
-
-	// Add finalizer for this CR
-	if !contains(pg.GetFinalizers(), pgServiceFinalizer) {
-		if err := r.addFinalizer(log, pg); err != nil {
-			return reconcile.Result{}, err
-		}
-	}
-
-	var aivenPG *aiven.Service
-
-	// Check if PG already exists on the Aiven side, create a
-	// new one if PG is not found
-	aivenPG, err = r.AivenClient.Services.Get(pg.Spec.Project, pg.Spec.ServiceName)
-	if err != nil {
-		// Create a new PG service if does not exists
-		if aiven.IsNotFound(err) {
-			_, err = r.createPGService(pg)
-			if err != nil {
-				log.Error(err, "Failed to create PG service")
-				return ctrl.Result{}, err
-			}
-
-			return ctrl.Result{
-				Requeue:      true,
-				RequeueAfter: 10 * time.Second,
-			}, nil
-		}
-		return ctrl.Result{}, err
-	}
-
-	// Check service status and wait until it is RUNNING
-	if aivenPG.State != "RUNNING" {
-		log.Info("PG service state is " + aivenPG.State + ", waiting to become RUNNING")
-		return ctrl.Result{
-			Requeue:      true,
-			RequeueAfter: 10 * time.Second,
-		}, nil
-	}
-
-	// Update PG service if it has differences between CR and Aiven representation
-	if r.hasDifferences(pg, aivenPG) {
-		_, err = r.updatePGService(pg, aivenPG)
-		if err != nil {
-			log.Error(err, "Failed to update PG service")
-			return ctrl.Result{}, err
-		}
-		return ctrl.Result{
-			Requeue:      true,
-			RequeueAfter: 10 * time.Second,
-		}, nil
-	}
-
-	err = r.updateCRStatus(pg, aivenPG)
-	if err != nil {
-		log.Error(err, "Failed to refresh PG service status")
-		return ctrl.Result{}, err
-	}
-
-	return ctrl.Result{}, nil
+	return r.reconcileInstance(&PGHandler{}, ctx, log, req, pg, pgServiceFinalizer)
 }
 
-// hasDifferences defines if there is a different between CR speck and Aiven representation of
-// PG service
-func (r *PGReconciler) hasDifferences(pg *k8soperatorv1alpha1.PG, s *aiven.Service) bool {
-	if pg.Spec.CloudName != s.CloudName {
-		return true
-	}
-	if pg.Spec.Plan != s.Plan {
-		return true
-	}
-	if pg.Spec.ServiceName != s.Name {
-		return true
-	}
-	if pg.Spec.MaintenanceWindowDow != s.MaintenanceWindow.DayOfWeek {
-		return true
-	}
-	if pg.Spec.MaintenanceWindowTime != s.MaintenanceWindow.TimeOfDay {
-		return true
-	}
-	if s.ProjectVPCID != nil && pg.Spec.Project != *s.ProjectVPCID {
-		return true
-	}
-
-	return false
+func (r *PGReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&k8soperatorv1alpha1.PG{}).
+		Complete(r)
 }
 
-// createPGService creates PG service and update CR status and creates secrets
-func (r *PGReconciler) createPGService(pg *k8soperatorv1alpha1.PG) (*aiven.Service, error) {
+func (h *PGHandler) exists(log logr.Logger, i client.Object) (bool, error) {
+	pg, err := h.convert(i)
+	if err != nil {
+		return false, err
+	}
+
+	log.Info("Checking if PG service already exists")
+
+	s, err := aivenClient.Services.Get(pg.Spec.Project, pg.Spec.ServiceName)
+	if aiven.IsNotFound(err) {
+		return false, nil
+	}
+
+	return s != nil, nil
+}
+
+// create creates PG service and update CR status and creates secrets
+func (h *PGHandler) create(log logr.Logger, i client.Object) (client.Object, error) {
+	pg, err := h.convert(i)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Info("Creating a new PG service")
+
 	var prVPCID *string
 	if pg.Spec.ProjectVPCID != "" {
 		prVPCID = &pg.Spec.ProjectVPCID
 	}
 
-	s, err := r.AivenClient.Services.Create(pg.Spec.Project, aiven.CreateServiceRequest{
+	s, err := aivenClient.Services.Create(pg.Spec.Project, aiven.CreateServiceRequest{
 		Cloud: pg.Spec.CloudName,
 		MaintenanceWindow: getMaintenanceWindow(
 			pg.Spec.MaintenanceWindowDow,
@@ -182,53 +88,46 @@ func (r *PGReconciler) createPGService(pg *k8soperatorv1alpha1.PG) (*aiven.Servi
 		return nil, err
 	}
 
-	// Update project custom resource status
-	err = r.updateCRStatus(pg, s)
-	if err != nil {
-		return nil, fmt.Errorf("failed to update PG service status %w", err)
-	}
+	h.setStatus(pg, s)
 
-	err = r.createPGSecret(pg, s)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create PG service secret %w", err)
-	}
-
-	return s, err
+	return pg, err
 }
 
-// updatePGService updates PG service and updates CR status
-func (r *PGReconciler) updatePGService(pg *k8soperatorv1alpha1.PG, s *aiven.Service) (*aiven.Service, error) {
+// update updates PG service and updates CR status
+func (h *PGHandler) update(log logr.Logger, i client.Object) (client.Object, error) {
+	pg, err := h.convert(i)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Info("Updating PG service")
+
 	var prVPCID *string
 	if pg.Spec.ProjectVPCID != "" {
 		prVPCID = &pg.Spec.ProjectVPCID
 	}
 
-	s, err := r.AivenClient.Services.Update(pg.Spec.Project, pg.Spec.ServiceName, aiven.UpdateServiceRequest{
+	s, err := aivenClient.Services.Update(pg.Spec.Project, pg.Spec.ServiceName, aiven.UpdateServiceRequest{
 		Cloud: pg.Spec.CloudName,
 		MaintenanceWindow: getMaintenanceWindow(
 			pg.Spec.MaintenanceWindowDow,
 			pg.Spec.MaintenanceWindowTime),
-		Plan:                  pg.Spec.Plan,
-		ProjectVPCID:          prVPCID,
-		TerminationProtection: s.TerminationProtection,
-		UserConfig:            UserConfigurationToAPI(pg.Spec.PGUserConfig).(map[string]interface{}),
-		Powered:               true,
+		Plan:         pg.Spec.Plan,
+		ProjectVPCID: prVPCID,
+		UserConfig:   UserConfigurationToAPI(pg.Spec.PGUserConfig).(map[string]interface{}),
+		Powered:      true,
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	// Update project custom resource status
-	err = r.updateCRStatus(pg, s)
-	if err != nil {
-		return nil, fmt.Errorf("failed to update PG service status %w", err)
-	}
+	h.setStatus(pg, s)
 
-	return s, err
+	return pg, nil
 }
 
-// updateCRStatus updates PG CR status
-func (r *PGReconciler) updateCRStatus(pg *k8soperatorv1alpha1.PG, s *aiven.Service) error {
+// setStatus updates PG CR status
+func (h *PGHandler) setStatus(pg *k8soperatorv1alpha1.PG, s *aiven.Service) {
 	var prVPCID string
 
 	if s.ProjectVPCID != nil {
@@ -242,57 +141,48 @@ func (r *PGReconciler) updateCRStatus(pg *k8soperatorv1alpha1.PG, s *aiven.Servi
 	pg.Status.MaintenanceWindowTime = s.MaintenanceWindow.TimeOfDay
 	pg.Status.MaintenanceWindowDow = s.MaintenanceWindow.DayOfWeek
 	pg.Status.CloudName = s.CloudName
-
-	err := r.Status().Update(context.Background(), pg)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
-// addFinalizer add finalizer to CR
-func (r *PGReconciler) addFinalizer(reqLogger logr.Logger, p *k8soperatorv1alpha1.PG) error {
-	reqLogger.Info("Adding Finalizer for the PG Service")
-	controllerutil.AddFinalizer(p, pgServiceFinalizer)
-
-	// Update CR
-	err := r.Client.Update(context.Background(), p)
+// delete deletes Aiven PG service
+func (h *PGHandler) delete(log logr.Logger, i client.Object) (client.Object, bool, error) {
+	pg, err := h.convert(i)
 	if err != nil {
-		reqLogger.Error(err, "Failed to update PG with finalizer")
-		return err
+		return nil, false, err
 	}
-	return nil
-}
 
-// finalizeProject deletes Aiven PG service
-func (r *PGReconciler) finalizePGService(log logr.Logger, p *k8soperatorv1alpha1.PG) error {
-	// Delete project on Aiven side
-	if err := r.AivenClient.Services.Delete(p.Spec.Project, p.Spec.ServiceName); err != nil {
+	// Delete PG on Aiven side
+	if err := aivenClient.Services.Delete(pg.Spec.Project, pg.Spec.ServiceName); err != nil {
 		if !aiven.IsNotFound(err) {
 			log.Error(err, "Cannot delete Aiven PG service")
-			return fmt.Errorf("aiven client delete PG error: %w", err)
+			return nil, false, fmt.Errorf("aiven client delete PG error: %w", err)
 		}
 	}
 
-	// Check if secret exists and delete if it is
-	secret := &corev1.Secret{}
-	err := r.Get(context.Background(), types.NamespacedName{Name: fmt.Sprintf("%s%s", p.Name, "-pg-secret"), Namespace: p.Namespace}, secret)
-	if err == nil {
-		err = r.Client.Delete(context.Background(), secret)
-		if err != nil {
-			return fmt.Errorf("delete project secret error: %w", err)
-		}
-	}
-
-	log.Info("Successfully finalized PG service")
-	return nil
+	log.Info("Successfully finalized PG service on Aiven side")
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s%s", pg.Name, "-pg-secret"),
+			Namespace: pg.Namespace,
+		},
+	}, true, nil
 }
 
-// createPGSecret creates a PG service secret
-func (r *PGReconciler) createPGSecret(pg *k8soperatorv1alpha1.PG, s *aiven.Service) error {
+// getSecret retrieves a PG service secret
+func (h *PGHandler) getSecret(log logr.Logger, i client.Object) (*corev1.Secret, error) {
+	pg, err := h.convert(i)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Info("Getting PG secret")
+
+	s, err := aivenClient.Services.Get(pg.Spec.Project, pg.Spec.ServiceName)
+	if err != nil {
+		return nil, fmt.Errorf("cannot get PG: %w", err)
+	}
+
 	params := s.URIParams
-	secret := &corev1.Secret{
+	return &corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      fmt.Sprintf("%s%s", pg.Name, "-pg-secret"),
 			Namespace: pg.Namespace,
@@ -306,23 +196,37 @@ func (r *PGReconciler) createPGSecret(pg *k8soperatorv1alpha1.PG, s *aiven.Servi
 			"password": params["password"],
 			"user":     params["user"],
 		},
-	}
-	err := r.Client.Create(context.Background(), secret)
-	if err != nil {
-		return fmt.Errorf("k8s client create error %w", err)
-	}
-
-	// Set PG service instance as the owner and controller
-	err = controllerutil.SetControllerReference(pg, secret, r.Scheme)
-	if err != nil {
-		return fmt.Errorf("k8s set controller error %w", err)
-	}
-
-	return nil
+	}, nil
 }
 
-func (r *PGReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&k8soperatorv1alpha1.PG{}).
-		Complete(r)
+func (h *PGHandler) isActive(log logr.Logger, i client.Object) (bool, error) {
+	pg, err := h.convert(i)
+	if err != nil {
+		return false, err
+	}
+
+	log.Info("Checking if PG service is active")
+
+	s, _ := aivenClient.Services.Get(pg.Spec.Project, pg.Spec.ServiceName)
+	if s == nil {
+		return false, nil
+
+	}
+
+	log.Info("PG state is " + s.State)
+
+	return s.State == "RUNNING", nil
+}
+
+func (h *PGHandler) convert(i client.Object) (*k8soperatorv1alpha1.PG, error) {
+	pg, ok := i.(*k8soperatorv1alpha1.PG)
+	if !ok {
+		return nil, fmt.Errorf("cannot convert object to PG")
+	}
+
+	return pg, nil
+}
+
+func (h *PGHandler) checkPreconditions(logr.Logger, client.Object) bool {
+	return true
 }
