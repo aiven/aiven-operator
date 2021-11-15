13d573fec2e1391103e34bea01d2c3c0d262097e
Ivan Savciuc <ivan.savciuc@gmail.com>
2021-11-15 19:35:41 +0200

    add support for redis service

---
+++ api/v1alpha1/redis_types.go
@@ -0,0 +1,184 @@
+// Copyright (c) 2021 Aiven, Helsinki, Finland. https://aiven.io/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// RedisSpec defines the desired state of Redis
+type RedisSpec struct {
+	ServiceCommonSpec `json:",inline"`
+
+	// Authentication reference to Aiven token in a secret
+	AuthSecretRef AuthSecretReference `json:"authSecretRef"`
+
+	// Information regarding secret creation
+	ConnInfoSecretTarget ConnInfoSecretTarget `json:"connInfoSecretTarget,omitempty"`
+
+	// Redis specific user configuration options
+	UserConfig RedisUserConfig `json:"userConfig,omitempty"`
+}
+
+type RedisPrivatelinkAccess struct {
+	// Enable redis
+	Redis *bool `json:"redis,omitempty"`
+}
+
+type RedisPublicAccess struct {
+	// Allow clients to connect to prometheus from the public *int64ernet for service nodes that are in a project VPC or another type of private network
+	Prometheus *bool `json:"prometheus,omitempty"`
+
+	// Allow clients to connect to redis from the public *int64ernet for service nodes that are in a project VPC or another type of private network
+	Redis *bool `json:"redis,omitempty"`
+}
+
+type RedisMigration struct {
+	// +kubebuilder:validation:MaxLength=2048
+	// Comma-separated list of databases, which should be ignored during migration (supported by MySQL only at the moment)
+	IgnoreDbs string `json:"ignore_dbs,omitempty"`
+
+	// +kubebuilder:validation:MaxLength=256
+	// Password for authentication with the server where to migrate data from
+	Password string `json:"password,omitempty"`
+
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=65535
+	// Port number of the server where to migrate data from
+	Port *int64 `json:"port,omitempty"`
+
+	// The server where to migrate data from is secured with SSL
+	Ssl *bool `json:"ssl,omitempty"`
+
+	// +kubebuilder:validation:MaxLength=256
+	// User name for authentication with the server where to migrate data from
+	Username string `json:"username,omitempty"`
+
+	// +kubebuilder:validation:MaxLength=63
+	// Database name for bootstrapping the initial connection
+	Dbname string `json:"dbname,omitempty"`
+
+	// +kubebuilder:validation:MaxLength=255
+	// Hostname or IP address of the server where to migrate data from
+	Host string `json:"host,omitempty"`
+}
+
+type RedisPrivateAccess struct {
+	// Allow clients to connect to prometheus with a DNS name that always resolves to the service's private IP addresses. Only available in certain network locations
+	Prometheus *bool `json:"prometheus,omitempty"`
+
+	// Allow clients to connect to redis with a DNS name that always resolves to the service's private IP addresses. Only available in certain network locations
+	Redis *bool `json:"redis,omitempty"`
+}
+
+type RedisUserConfig struct {
+	// Migrate data from existing server
+	Migration RedisMigration `json:"migration,omitempty"`
+
+	// Allow access to selected service ports from the public *int64ernet
+	PublicAccess RedisPublicAccess `json:"public_access,omitempty"`
+
+	// Allow access to selected service ports from private networks
+	PrivateAccess RedisPrivateAccess `json:"private_access,omitempty"`
+
+	// Allow access to selected service components through Privatelink
+	PrivatelinkAccess RedisPrivatelinkAccess `json:"privatelink_access,omitempty"`
+
+	// +kubebuilder:validation:MaxLength=63
+	// Name of another service to fork from. This has effect only when a new service is being created.
+	ServiceToForkFrom string `json:"service_to_fork_from,omitempty"`
+
+	// IP filter Allow incoming connections from CIDR address block, e.g. '10.20.0.0/16'
+	IPFilter []string `json:"ip_filter,omitempty"`
+
+	// +kubebuilder:validation:MaxLength=63
+	// Name of another project to fork a service from. This has effect only when a new service is being created.
+	ProjectToForkFrom string `json:"project_to_fork_from,omitempty"`
+
+	// +kubebuilder:validation:Enum=allchannels;resetchannels
+	// Default ACL for pub/sub channels used when Redis user is created Determines default pub/sub channels' ACL for new users if ACL is not supplied. When this option is not defined, all_channels is assumed to keep backward compatibility. This option doesn't affect Redis configuration acl-pubsub-default.
+	RedisAclChannelsDefault string `json:"redis_acl_channels_default,omitempty"`
+
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=120
+	// LFU maxmemory-policy counter decay time in minutes
+	RedisLfuDecayTime *int64 `json:"redis_lfu_decay_time,omitempty"`
+
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Maximum=100
+	// Counter logarithm factor for volatile-lfu and allkeys-lfu maxmemory-policies
+	RedisLfuLogFactor *int64 `json:"redis_lfu_log_factor,omitempty"`
+
+	// +kubebuilder:validation:Enum=off;rdb
+	// Redis persistence When persistence is 'rdb', Redis does RDB dumps each 10 minutes if any key is changed. Also RDB dumps are done according to backup schedule for backup purposes. When persistence is 'off', no RDB dumps and backups are done, so data can be lost at any moment if service is restarted for any reason, or if service is powered off. Also service can't be forked.
+	RedisPersistence string `json:"redis_persistence,omitempty"`
+
+	// +kubebuilder:validation:Minimum=32
+	// +kubebuilder:validation:Maximum=512
+	// Pub/sub client output buffer hard limit in MB Set output buffer limit for pub / sub clients in MB. The value is the hard limit, the soft limit is 1/4 of the hard limit. When setting the limit, be mindful of the available memory in the selected service plan.
+	RedisPubsubClientOutputBufferLimit *int64 `json:"redis_pubsub_client_output_buffer_limit,omitempty"`
+
+	// Static IP addresses Use static public IP addresses
+	StaticIps *bool `json:"static_ips,omitempty"`
+
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=32
+	// Redis IO thread count
+	RedisIoThreads *int64 `json:"redis_io_threads,omitempty"`
+
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Maximum=31536000
+	// Redis idle connection timeout
+	RedisTimeout *int64 `json:"redis_timeout,omitempty"`
+
+	// +kubebuilder:validation:Format="^[a-zA-Z0-9-_:.]+$"
+	// +kubebuilder:validation:MaxLength=128
+	// Name of the basebackup to restore in forked service
+	RecoveryBasebackupName string `json:"recovery_basebackup_name,omitempty"`
+
+	// +kubebuilder:validation:Enum=noeviction;allkeys-lru;volatile-lru;allkeys-random;volatile-random;volatile-ttl;volatile-lfu;allkeys-lfu
+	// Redis maxmemory-policy
+	RedisMaxmemoryPolicy string `json:"redis_maxmemory_policy,omitempty"`
+
+	// +kubebuilder:validation:MaxLength=32
+	// Set notify-keyspace-events option
+	RedisNotifyKeyspaceEvents string `json:"redis_notify_keyspace_events,omitempty"`
+
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=128
+	// Number of redis databases Set number of redis databases. Changing this will cause a restart of redis service.
+	RedisNumberOfDatabases *int64 `json:"redis_number_of_databases,omitempty"`
+
+	// Require SSL to access Redis
+	RedisSsl *bool `json:"redis_ssl,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Redis is the Schema for the redis API
+// +kubebuilder:subresource:status
+type Redis struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RedisSpec     `json:"spec,omitempty"`
+	Status ServiceStatus `json:"status,omitempty"`
+}
+
+func (r Redis) AuthSecretRef() AuthSecretReference {
+	return r.Spec.AuthSecretRef
+}
+
+//+kubebuilder:object:root=true
+
+// RedisList contains a list of Redis
+type RedisList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Redis `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Redis{}, &RedisList{})
+}
---
+++ controllers/redis_controller.go
@@ -0,0 +1,184 @@
+// Copyright (c) 2021 Aiven, Helsinki, Finland. https://aiven.io/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/aiven/aiven-go-client"
+	"github.com/aiven/aiven-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// RedisReconciler reconciles a Redis object
+type RedisReconciler struct {
+	Controller
+}
+
+type RedisHandler struct{}
+
+//+kubebuilder:rbac:groups=aiven.io,resources=redis,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=aiven.io,resources=redis/status,verbs=get;update;patch
+
+func (r *RedisReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	return r.reconcileInstance(ctx, req, RedisHandler{}, &v1alpha1.Redis{})
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *RedisReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&v1alpha1.Redis{}).
+		Owns(&corev1.Secret{}).
+		Complete(r)
+}
+
+func (h RedisHandler) createOrUpdate(avn *aiven.Client, i client.Object) error {
+	redis, err := h.convert(i)
+	if err != nil {
+		return err
+	}
+
+	var prVPCID *string
+	if redis.Spec.ProjectVPCID != "" {
+		prVPCID = &redis.Spec.ProjectVPCID
+	}
+
+	exists, err := h.exists(avn, redis)
+	if err != nil {
+		return err
+	}
+
+	var reason string
+	if !exists {
+		_, err := avn.Services.Create(redis.Spec.Project, aiven.CreateServiceRequest{
+			Cloud: redis.Spec.CloudName,
+			MaintenanceWindow: getMaintenanceWindow(
+				redis.Spec.MaintenanceWindowDow,
+				redis.Spec.MaintenanceWindowTime),
+			Plan:                redis.Spec.Plan,
+			ProjectVPCID:        prVPCID,
+			ServiceName:         redis.Name,
+			ServiceType:         "redis",
+			UserConfig:          UserConfigurationToAPI(redis.Spec.UserConfig).(map[string]interface{}),
+			ServiceIntegrations: nil,
+		})
+		if err != nil {
+			return err
+		}
+
+		reason = "Created"
+	} else {
+		_, err := avn.Services.Update(redis.Spec.Project, redis.Name, aiven.UpdateServiceRequest{
+			Cloud: redis.Spec.CloudName,
+			MaintenanceWindow: getMaintenanceWindow(
+				redis.Spec.MaintenanceWindowDow,
+				redis.Spec.MaintenanceWindowTime),
+			Plan:         redis.Spec.Plan,
+			ProjectVPCID: prVPCID,
+			UserConfig:   UserConfigurationToAPI(redis.Spec.UserConfig).(map[string]interface{}),
+			Powered:      true,
+		})
+		if err != nil {
+			return err
+		}
+
+		reason = "Updated"
+	}
+
+	meta.SetStatusCondition(&redis.Status.Conditions,
+		getInitializedCondition(reason,
+			"Instance was created or update on Aiven side"))
+
+	meta.SetStatusCondition(&redis.Status.Conditions,
+		getRunningCondition(metav1.ConditionUnknown, reason,
+			"Instance was created or update on Aiven side, status remains unknown"))
+
+	metav1.SetMetaDataAnnotation(&redis.ObjectMeta,
+		processedGenerationAnnotation, strconv.FormatInt(redis.GetGeneration(), formatIntBaseDecimal))
+
+	return nil
+}
+
+func (h RedisHandler) convert(o client.Object) (*v1alpha1.Redis, error) {
+	r, ok := o.(*v1alpha1.Redis)
+	if !ok {
+		return nil, fmt.Errorf("cannot convert object to Redis")
+	}
+	return r, nil
+}
+
+func (h RedisHandler) exists(avn *aiven.Client, r *v1alpha1.Redis) (bool, error) {
+	s, err := avn.Services.Get(r.Spec.Project, r.Name)
+	if aiven.IsNotFound(err) {
+		return false, nil
+	}
+
+	return s != nil, nil
+}
+
+func (h RedisHandler) delete(avn *aiven.Client, i client.Object) (bool, error) {
+	redis, err := h.convert(i)
+	if err != nil {
+		return false, err
+	}
+
+	// Delete PostgreSQL on Aiven side
+	if err := avn.Services.Delete(redis.Spec.Project, redis.Name); err != nil && !aiven.IsNotFound(err) {
+		return false, fmt.Errorf("aiven client delete redis error: %w", err)
+	}
+
+	return true, nil
+}
+
+func (h RedisHandler) get(avn *aiven.Client, i client.Object) (*corev1.Secret, error) {
+	redis, err := h.convert(i)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := avn.Services.Get(redis.Spec.Project, redis.Name)
+	if err != nil {
+		return nil, fmt.Errorf("cannot get redis: %w", err)
+	}
+
+	redis.Status.State = s.State
+
+	if s.State == "RUNNING" {
+		meta.SetStatusCondition(&redis.Status.Conditions,
+			getRunningCondition(metav1.ConditionTrue, "CheckRunning",
+				"Instance is running on Aiven side"))
+
+		metav1.SetMetaDataAnnotation(&redis.ObjectMeta, instanceIsRunningAnnotation, "true")
+	}
+
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      h.getSecretName(redis),
+			Namespace: redis.Namespace,
+		},
+		StringData: map[string]string{
+			"HOST":     s.URIParams["host"],
+			"PASSWORD": s.URIParams["password"],
+			"PORT":     s.URIParams["port"],
+			"SSL":      s.URIParams["ssl"],
+			"USER":     s.URIParams["user"],
+		},
+	}, nil
+}
+
+func (h RedisHandler) getSecretName(redis *v1alpha1.Redis) string {
+	if redis.Spec.ConnInfoSecretTarget.Name != "" {
+		return redis.Spec.ConnInfoSecretTarget.Name
+	}
+	return redis.Name
+}
+
+func (h RedisHandler) checkPreconditions(_ *aiven.Client, _ client.Object) (bool, error) {
+	return true, nil
+}
