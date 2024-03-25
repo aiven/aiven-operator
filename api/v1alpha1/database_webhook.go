47f1c1a65a15244d49e35e15f6143694b6f641f1
Murad Biashimov <murad.biashimov@aiven.io>
2024-03-25 17:34:10 +0100

    refactor(database): use crd validation rules (#682)

--- api/v1alpha1/database_webhook.go
+++ api/v1alpha1/database_webhook.go
@@ -27,16 +27,6 @@ var _ webhook.Defaulter = &Database{}
 // Default implements webhook.Defaulter so a webhook will be registered for the type
 func (in *Database) Default() {
 	databaselog.Info("default", "name", in.Name)
-
-	const defaultLC = "en_US.UTF-8"
-
-	if in.Spec.LcCtype == "" {
-		in.Spec.LcCtype = defaultLC
-	}
-
-	if in.Spec.LcCollate == "" {
-		in.Spec.LcCollate = defaultLC
-	}
 }
 
 //+kubebuilder:webhook:verbs=create;update;delete,path=/validate-aiven-io-v1alpha1-database,mutating=false,failurePolicy=fail,groups=aiven.io,resources=databases,versions=v1alpha1,name=vdatabase.kb.io,sideEffects=none,admissionReviewVersions=v1
@@ -53,22 +43,6 @@ func (in *Database) ValidateCreate() error {
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (in *Database) ValidateUpdate(old runtime.Object) error {
 	databaselog.Info("validate update", "name", in.Name)
-
-	if in.Spec.Project != old.(*Database).Spec.Project {
-		return errors.New("cannot update a Database, project field is immutable and cannot be updated")
-	}
-
-	if in.Spec.ServiceName != old.(*Database).Spec.ServiceName {
-		return errors.New("cannot update a Database, service_name field is immutable and cannot be updated")
-	}
-
-	if in.Spec.LcCollate != old.(*Database).Spec.LcCollate {
-		return errors.New("cannot update a Database, lc_collate field is immutable and cannot be updated")
-	}
-
-	if in.Spec.LcCtype != old.(*Database).Spec.LcCtype {
-		return errors.New("cannot update a Database, lc_ctype field is immutable and cannot be updated")
-	}
 
 	return nil
 }
