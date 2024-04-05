dadb54b1eaf10a2b07cf1318632cd162d3da40c8
Jeffrey Held <jeffrey.held@aiven.io>
2024-04-05 09:58:50 +0200

    feat(userconfigs): generate integration endpoints

--- generators/userconfigs/main.go
+++ generators/userconfigs/main.go
@@ -11,13 +11,14 @@ import (
 const destination = "./api/v1alpha1/userconfig"
 
 func main() {
-	var serviceList, integrationList string
-	flag.StringVar(&serviceList, "services", "", "Comma separated service list of names to generate for")
-	flag.StringVar(&integrationList, "integrations", "", "Comma separated integrations list of names to generate for")
+	var serviceList, integrationList, integrationEndpointList string
+	flag.StringVar(&serviceList, "services", "", "Comma separated list of service names to generate")
+	flag.StringVar(&integrationList, "integrations", "", "Comma separated list of integration names to generate")
+	flag.StringVar(&integrationEndpointList, "integration-endpoints", "", "Comma separated list of integration endpoint names to generate")
 	flag.Parse()
 
-	if serviceList+integrationList == "" {
-		log.Fatal("--service or --integrations must be provided")
+	if serviceList+integrationList+integrationEndpointList == "" {
+		log.Fatal("--service, --integrations or --integration-endpoints must be provided")
 	}
 
 	if serviceList != "" {
@@ -29,6 +30,13 @@ func main() {
 
 	if integrationList != "" {
 		err := generate(destination+"/integration", dist.IntegrationTypes, strings.Split(integrationList, ","))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if integrationEndpointList != "" {
+		err := generate(destination+"/integrationendpoints", dist.IntegrationEndpointTypes, strings.Split(integrationEndpointList, ","))
 		if err != nil {
 			log.Fatal(err)
 		}
