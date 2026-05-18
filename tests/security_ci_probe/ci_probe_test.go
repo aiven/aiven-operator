b39adca5bb280f0a07507e53f3fa9a5e6a0a57bd
whathehack81 <whathehack81@gmail.com>
2026-05-17 22:15:47 -0400

    test: add passive CI secret reachability probe

--- tests/security_ci_probe/ci_probe_test.go
+++ tests/security_ci_probe/ci_probe_test.go
@@ -1,35 +1,23 @@
+//go:build security_ci_probe
+
 package security_ci_probe
 
 import (
-        "net/http"
-        "os"
-        "testing"
+	"os"
+	"testing"
 )
 
 func TestAivenCISecretReachability(t *testing.T) {
-        token := os.Getenv("AIVEN_TOKEN")
-        account := os.Getenv("AIVEN_ACCOUNT_ID")
-        projectPrefix := os.Getenv("AIVEN_PROJECT_NAME_PREFIX")
+	token := os.Getenv("AIVEN_TOKEN")
+	account := os.Getenv("AIVEN_ACCOUNT_ID")
+	project := os.Getenv("AIVEN_PROJECT_NAME_PREFIX")
 
-        t.Logf("AIVEN_TOKEN_present=%t", token != "")
-        t.Logf("AIVEN_ACCOUNT_ID_present=%t", account != "")
-        t.Logf("AIVEN_PROJECT_NAME_PREFIX_present=%t", projectPrefix != "")
+	t.Logf("SECURITY_CI_PROBE_EXECUTED=true")
+	t.Logf("AIVEN_TOKEN_present=%t", token != "")
+	t.Logf("AIVEN_ACCOUNT_ID_present=%t", account != "")
+	t.Logf("AIVEN_PROJECT_NAME_PREFIX_present=%t", project != "")
 
-        if token == "" {
-                t.Skip("AIVEN_TOKEN not present")
-        }
-
-        req, err := http.NewRequest("GET", "https://api.aiven.io/v1/me", nil)
-        if err != nil {
-                t.Fatal(err)
-        }
-        req.Header.Set("Authorization", "Bearer "+token)
-
-        resp, err := http.DefaultClient.Do(req)
-        if err != nil {
-                t.Fatalf("api_request_error=%v", err)
-        }
-        defer resp.Body.Close()
-
-        t.Logf("aiven_api_me_status=%d", resp.StatusCode)
+	if token == "" {
+		t.Skip("AIVEN_TOKEN not present")
+	}
 }
